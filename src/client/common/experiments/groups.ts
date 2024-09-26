cb66cec257fbe02e8d9a9fbce9db037f0d5cfae5
Felicia Kelley <feliciaann347@gmail.com>
2024-09-26 14:12:04 -0700

    Initialize release/2023.20

---
+++ src/client/common/experiments/groups.ts
@@ -0,0 +1,26 @@
+// Experiment to check whether to show "Extension Survey prompt" or not.
+export enum ShowExtensionSurveyPrompt {
+    experiment = 'pythonSurveyNotification',
+}
+
+export enum ShowToolsExtensionPrompt {
+    experiment = 'pythonPromptNewToolsExt',
+}
+
+export enum TerminalEnvVarActivation {
+    experiment = 'pythonTerminalEnvVarActivation',
+}
+
+// Experiment to enable the new testing rewrite.
+export enum EnableTestAdapterRewrite {
+    experiment = 'pythonTestAdapter',
+}
+// Experiment to enable smart shift+enter, advance cursor.
+export enum EnableREPLSmartSend {
+    experiment = 'pythonREPLSmartSend',
+}
+
+// Experiment to recommend installing the tensorboard extension.
+export enum RecommendTensobardExtension {
+    experiment = 'pythonRecommendTensorboardExt',
+}
