3719b5fa9341e94d23eb87755e3675c2d672cbd8
Felicia Kelley <feliciaann347@gmail.com>
2024-09-26 14:11:52 -0700

    Initialize deprecateLogDirectory

---
+++ src/client/common/installer/productNames.ts
@@ -0,0 +1,20 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+import { Product } from '../types';
+
+export const ProductNames = new Map<Product, string>();
+ProductNames.set(Product.bandit, 'bandit');
+ProductNames.set(Product.flake8, 'flake8');
+ProductNames.set(Product.mypy, 'mypy');
+ProductNames.set(Product.pycodestyle, 'pycodestyle');
+ProductNames.set(Product.pylama, 'pylama');
+ProductNames.set(Product.prospector, 'prospector');
+ProductNames.set(Product.pydocstyle, 'pydocstyle');
+ProductNames.set(Product.pylint, 'pylint');
+ProductNames.set(Product.pytest, 'pytest');
+ProductNames.set(Product.tensorboard, 'tensorboard');
+ProductNames.set(Product.torchProfilerInstallName, 'torch-tb-profiler');
+ProductNames.set(Product.torchProfilerImportName, 'torch_tb_profiler');
+ProductNames.set(Product.pip, 'pip');
+ProductNames.set(Product.ensurepip, 'ensurepip');
